adad7e203b448f60455da4a5f563cee4be98b9bb
Es-machte-SCHLONK <151451535+Es-machte-SCHLONK@users.noreply.github.com>
2024-01-01 14:58:47 +0100

    Formatting & Mockup Screen for GUI

--- lib/map_surface/map_surface.py
+++ lib/map_surface/map_surface.py
@@ -10,10 +10,8 @@ class Maps:
         self.root_width = root_screen.get_width()
         self.init_surface()
 
-
     def init_surface(self):
         self.surface = pyg.Surface((self.root_width, (int(self.root_height * 0.8))))
         self.surface.fill(self.map_color)
         nodelist = nl.NodeList(10)
         nodelist.draw_node(self.surface)
-
