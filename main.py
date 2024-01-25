5bad2749efb23684272ca125cf2280ccf08e0a16
schwabec <schwabec@th-brandenburg.de>
2024-01-25 23:56:22 +0100

    change player

--- main.py
+++ main.py
@@ -211,7 +211,7 @@ class Game:
         self.players.players[index].current_position = self.selected_position
         self.players.players[index].on_turn = False
         self.players.players[new_index].on_turn = True
-
+        self.map.init_surface(self.root)
         self.players.init_surface()
 
 
